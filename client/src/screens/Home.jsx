b6970ee5c7526ac737ba831d2f74fb2477e267b6
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-11 17:40:04 -0500

    adds signin component

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import SignIn from './components/SignIn'
+
 
 export default function Home() {
+
   return (
     <div>
       <h1>Home</h1>
+      <SignIn />
     </div>
   )
 }
