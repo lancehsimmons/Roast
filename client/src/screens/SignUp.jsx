4abf76d54b485d74ffceb1b8b1110973e7608643
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-11 17:48:47 -0500

    sign in form renders on home

--- client/src/screens/SignUp.jsx
+++ client/src/screens/SignUp.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+
 
 export default function SignUp() {
+
   return (
     <div>
       <h4>sign up</h4>
