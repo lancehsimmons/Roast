93f4b3a11e5df11c0120d442163d0e791da41e8b
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-12 13:04:07 -0500

    adds link to each coffee on List

--- client/src/screens/List.jsx
+++ client/src/screens/List.jsx
@@ -4,8 +4,12 @@ import { Link } from 'react-router-dom'
 export default function List({coffees}) {
   return (
     <div>
-      {coffees.map((coffee) => 
-        <p>{coffee.name}</p>
+      {coffees.map((coffee) =>
+        <div>
+          <Link to={`/coffees/${coffee.id}`}>
+            <p>{coffee.name}</p>
+          </Link>
+        </div>
       )}
            <Link to='/add-roast'>
         <button>Add Roast</button>
