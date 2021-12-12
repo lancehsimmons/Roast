5eaeb2699fe224059d54969ea13dc6ac6c8d440c
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-12 11:37:43 -0500

    adds state and handlechange to add roast

--- client/src/screens/AddRoast.jsx
+++ client/src/screens/AddRoast.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function AddRoast() {
+  const [formData, setFormData] = useState({
+    name: '',
+  });
+  const { name } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <h1>add a roast here</h1>
