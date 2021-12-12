b1f7016db5a95033c864f99e5b81b68abd9f88fb
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-11 23:40:21 -0500

    coffee names mapping on list through root

--- client/src/screens/List.jsx
+++ client/src/screens/List.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function List({coffees}) {
   return (
     <div>
-      {/* <h3>{coffees[0].name}</h3> */}
+      {coffees.map((coffee) => 
+        <p>{coffee.name}</p>
+      )}
       <h3>coffees</h3>
     </div>
   )
