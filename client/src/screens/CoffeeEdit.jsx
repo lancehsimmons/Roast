1470060b424e9e7c9153faabf6c0a943567a566c
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-12 14:52:55 -0500

    adds coffee edit w params id and form state

--- client/src/screens/CoffeeEdit.jsx
+++ client/src/screens/CoffeeEdit.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function CoffeeEdit() {
+  const [formData, setFormData] = useState({
+    name: '',
+    roast_time: '',
+    roast_level: '',
+    roaster_settings: '',
+    preheat: '',
+    yellowing: '',
+    browning: '',
+    first_crack: '',
+    second_crack: '',
+    first_crack_end: '',
+    end_drop: '',
+    notes: '',
+  });
+  const {
+    name,
+    roast_time,
+    roast_level,
+    roaster_settings,
+    preheat,
+    yellowing,
+    browning,
+    first_crack,
+    second_crack,
+    first_crack_end,
+    end_drop,
+    notes,
+  } = formData;
   return (
     <div>
       <p>a form to edit a roast</p>
