a8c1e9a3b923bec9f047048ac1073b34f0e25be5
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-13 21:45:56 -0500

    detail card getting styled

--- client/src/screens/CoffeeDetail.jsx
+++ client/src/screens/CoffeeDetail.jsx
@@ -1,3 +1,4 @@
+import '../assets/coffeedetail.css'
 import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 
@@ -18,57 +19,69 @@ export default function CoffeeDetail({ currentUser }) {
 
   return (
     <div>
-      <label>
-        Name:
-      <p>{coffee?.name}</p>
-      </label>
-      <label>
-        Roast Time:
-      <p>{coffee?.roast_time}</p>
-      </label>
-      <label>
-        Roast Level:
-      <p>{coffee?.roast_level}</p>
-      </label>
-      <label>
-        Roaster Settings:
-      <p>{coffee?.roaster_settings}</p>
-      </label>
-      <label>
-        Preheat Time/Temp:
-      <p>{coffee?.preheat}</p>
-      </label>
-      <label>
-        Yellowing:
-      <p>{coffee?.yellowing}</p>
-      </label>
-      <label>
-        Browning:
-      <p>{coffee?.browning}</p>
-      </label>
-      <label>
-        First Crack:
-      <p>{coffee?.first_crack}</p>
-      </label>
-      <label>
-        Second Crack:
-      <p>{coffee?.second_crack}</p>
-      </label>
-      <label>
-        First Crack Ends:
-      <p>{coffee?.first_crack_end}</p>
-      </label>
-      <label>
-        End/Drop:
-      <p>{coffee?.end_drop}</p>
-      </label>
-      <label>
-        Notes:
-      <p>{coffee?.notes}</p>
-      </label>
       <Link to={`/coffees/${coffee?.id}/edit`}>
-        <button>Edit</button>
+        <button className='edit-button'>EDIT</button>
       </Link>
+
+      <div className='detail-div'>
+        <div className='list-title'>
+          <div><h5>COFFEE</h5></div>
+          <div className='date-div'>
+            <h5 className='date'>
+              DATE: {coffee?.created_at.slice(0, 10)}
+            </h5>
+          </div>
+        </div>
+
+        <p>{coffee?.name}</p>
+        <hr />
+        <label>
+
+          <p>Roast Time:    {coffee?.roast_time}</p>
+        </label>
+        <label>
+          Roast Level:
+          <p>{coffee?.roast_level}</p>
+        </label>
+        <label>
+          Roaster Settings:
+          <p>{coffee?.roaster_settings}</p>
+        </label>
+        <label>
+          Preheat Time/Temp:
+          <p>{coffee?.preheat}</p>
+        </label>
+        <label>
+          Yellowing:
+          <p>{coffee?.yellowing}</p>
+        </label>
+        <label>
+          Browning:
+          <p>{coffee?.browning}</p>
+        </label>
+        <label>
+          First Crack:
+          <p>{coffee?.first_crack}</p>
+        </label>
+        <label>
+          Second Crack:
+          <p>{coffee?.second_crack}</p>
+        </label>
+        <label>
+          First Crack Ends:
+          <p>{coffee?.first_crack_end}</p>
+        </label>
+        <label>
+          End/Drop:
+          <p>{coffee?.end_drop}</p>
+        </label>
+        <label>
+          Notes:
+          <p>{coffee?.notes}</p>
+        </label>
+      </div>
+
+
     </div>
   )
 }
