e2a54ec4ca5a7680e60a92159f76af50467f8ddf
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-13 20:22:44 -0500

    formatting styling list cards

--- client/src/screens/List.jsx
+++ client/src/screens/List.jsx
@@ -5,21 +5,26 @@ import '../assets/list.css'
 export default function List({ coffees, handleCoffeeDelete}) {
   return (
     <div>
+              <button className='add-button'>ADD ROAST</button>
+
       <div className='list-div'>
       {coffees.map((coffee) =>
         <div className="list-one-div">
-          <h5>COFFEE</h5>
+          <div className='list-title'>
+
+            <div><h5>COFFEE</h5></div>
+            <div className='date-div'><h5>DATE: {coffee.created_at}</h5></div>
+          </div>
           <Link to={`/coffees/${coffee.id}`}>
-            <p>{coffee.name}</p>
+            <p className='name'>{coffee.name}</p>
           </Link>
-          <button onClick={() => handleCoffeeDelete(coffee.id)}>
+          <button className='delete' onClick={() => handleCoffeeDelete(coffee.id)}>
             DEL
           </button>
         </div>
       )}
     </div>
       <Link to='/add-roast'>
-        <button>Add Roast</button>
       </Link>
     </div>
   )
