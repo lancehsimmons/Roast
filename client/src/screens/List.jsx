5529f77f58ccfe0f5db5e2ec2876ecd138fe37cb
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-13 19:57:13 -0500

    adds list card divs and coffee title with styling

--- client/src/screens/List.jsx
+++ client/src/screens/List.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../assets/list.css'
 
 export default function List({ coffees, handleCoffeeDelete}) {
   return (
     <div>
+      <div className='list-div'>
       {coffees.map((coffee) =>
-        <div>
+        <div className="list-one-div">
+          <h5>COFFEE</h5>
           <Link to={`/coffees/${coffee.id}`}>
             <p>{coffee.name}</p>
           </Link>
           <button onClick={() => handleCoffeeDelete(coffee.id)}>
-            Delete
+            DEL
           </button>
         </div>
       )}
+    </div>
       <Link to='/add-roast'>
         <button>Add Roast</button>
       </Link>
