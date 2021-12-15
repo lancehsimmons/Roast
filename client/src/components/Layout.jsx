c6ac0e89dd82b9a08d76ad9e5a07ed0822174a0e
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-15 14:17:11 -0500

    adds media query for nav flex directon

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -10,32 +10,35 @@ import '../assets/layout.css'
 export default function Layout({ currentUser, handleLogout, children }) {
   const location = useLocation()
 
- 
+
 
   return (
     <div>
       <>
-        {currentUser ? 
+        {currentUser ?
           <>
-          <nav>
-              <img className='logo2' src='../images/roast_logo2.png' alt='a coffee alchemist' />
-              
-            <div classname='welcome-div'>
-              <h2 className='welcome'>welcome, {currentUser.username}</h2>
-            </div>
-    
-            <div className='nav-item' onClick={handleLogout}><h3>Logout</h3>
-            </div>
-    
-            <div className='nav-item'>
+            <nav>
               <NavLink to='/'>
-                <h3>Home</h3>
+                <img className='logo2' src='../images/roast_logo2.png' alt='a coffee alchemist' />
               </NavLink>
-            </div>
-          </nav>
-        </> :
+
+
+              <div classname='welcome-div'>
+                <h2 className='welcome'>welcome, {currentUser.username}</h2>
+              </div>
+
+              <div className='nav-item' onClick={handleLogout}><h3>Logout</h3>
+              </div>
+
+              <div className='nav-item'>
+                <NavLink to='/'>
+                  <h3>Home</h3>
+                </NavLink>
+              </div>
+            </nav>
+          </> :
           null}
-        </>
+      </>
       {children}
     </div>
   )
