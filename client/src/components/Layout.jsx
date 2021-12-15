34c96241ff8f2507d2b2b8f73bbe627fa82c9eb1
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-15 14:35:39 -0500

    media query nav styling

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -17,25 +17,27 @@ export default function Layout({ currentUser, handleLogout, children }) {
       <>
         {currentUser ?
           <>
-            <nav>
-              <NavLink to='/'>
-                <img className='logo2' src='../images/roast_logo2.png' alt='a coffee alchemist' />
-              </NavLink>
-
-
-              <div classname='welcome-div'>
-                <h2 className='welcome'>welcome, {currentUser.username}</h2>
-              </div>
-
-              <div className='nav-item' onClick={handleLogout}><h3>Logout</h3>
-              </div>
-
-              <div className='nav-item'>
+            <div className='nav-div'>
+              <nav>
                 <NavLink to='/'>
-                  <h3>Home</h3>
+                  <img className='logo2' src='../images/roast_logo2.png' alt='a coffee alchemist' />
                 </NavLink>
-              </div>
-            </nav>
+
+
+                <div classname='welcome-div'>
+                  <h2 className='welcome'>Welcome, {currentUser.username}</h2>
+                </div>
+
+                <div className='nav-item' onClick={handleLogout}><h3>Logout</h3>
+                </div>
+
+                <div className='nav-item'>
+                  <NavLink to='/'>
+                    <h3>Home</h3>
+                  </NavLink>
+                </div>
+              </nav>
+            </div>
           </> :
           null}
       </>
